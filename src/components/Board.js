03a7efcf2e44660a92afc23c951eb9ac812847e3
jackdbd <jackdebidda@gmail.com>
2018-05-14 13:01:06 +0200

    Refactor Board into a functional component

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,31 +2,29 @@
  * Board module.
  * @module /components/Board
  */
-import React, { Component } from "react";
+import React from "react";
 import BoardRow from "./BoardRow";
 
-class Board extends Component {
-  render() {
-    return (
-      <div>
-        <BoardRow
-          indexStart={0}
-          squares={this.props.squares}
-          onClick={this.props.onClick}
-        />
-        <BoardRow
-          indexStart={3}
-          squares={this.props.squares}
-          onClick={this.props.onClick}
-        />
-        <BoardRow
-          indexStart={6}
-          squares={this.props.squares}
-          onClick={this.props.onClick}
-        />
-      </div>
-    );
-  }
-}
+const Board = props => {
+  return (
+    <div>
+      <BoardRow
+        indexStart={0}
+        squares={props.squares}
+        onClick={props.onClick}
+      />
+      <BoardRow
+        indexStart={3}
+        squares={props.squares}
+        onClick={props.onClick}
+      />
+      <BoardRow
+        indexStart={6}
+        squares={props.squares}
+        onClick={props.onClick}
+      />
+    </div>
+  );
+};
 
 export default Board;
