8d571186f267f06c6c3e9423ed4de65611c7bb1b
jackdbd <jackdebidda@gmail.com>
2018-05-14 15:07:54 +0200

    Add d3

--- src/components/BoardRow.js
+++ src/components/BoardRow.js
@@ -3,6 +3,7 @@
  * @module /components/BoardRow
  */
 import React from "react";
+import * as d3 from "d3";
 import BoardCell from "./BoardCell";
 import styled from "styled-components";
 
@@ -16,7 +17,7 @@ const Div = styled.div`
 
 const BoardRow = props => {
   const { indexStart, cells, onClick } = props;
-  const BoardCellList = [0, 1, 2].map(index => {
+  const BoardCellList = d3.range(3).map(index => {
     const i = indexStart + index;
     return (
       <BoardCell key={index} value={cells[i]} onClick={() => onClick(i)} />
