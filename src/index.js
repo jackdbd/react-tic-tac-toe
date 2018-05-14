8263cc2bc382b8771703ffc225219cb6e709c761
jackdbd <jackdebidda@gmail.com>
2018-05-14 10:36:23 +0200

    Move Board component to its own module

--- src/index.js
+++ src/index.js
@@ -1,41 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { calculateWinner } from "./utils";
-import { Square } from "./components/Square";
+import Board from "./components/Board";
 import "./index.css";
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
 
 class Game extends React.Component {
   constructor(props) {
