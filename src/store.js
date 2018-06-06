2e3b2e4751d6e1b7a8354fd74e6dc22ace086b4e
jackdbd <jackdebidda@gmail.com>
2018-06-06 13:30:15 +0200

    Updates

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "redux-devtools-extension/logOnlyInProduction";
-import rootReducer from "./reducers";
-
-const storeEnhancer = devToolsEnhancer();
-const store = createStore(rootReducer, storeEnhancer);
-
-export default store;
